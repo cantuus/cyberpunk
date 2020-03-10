ee9b712e127dbbb5268518e29e8f275c63816b25
Sophia Koeut <koeutsophia@gmail.com>
2020-03-09 19:25:48 -0700

    Initialize cyberpunk react project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="cyber-container">
+        <div className="h-container">
+          <h1 className="cyber-title glitch">come my einherjars, battle awaits us_</h1>
+          <h1 className="cyber-title glitch">come my einherjars, battle awaits us_</h1>
+          <h1 className="cyber-title glitch">come my einherjars, battle awaits us_</h1>
+        </div>
+        <div className="button-container">
+          <button className="btn">
+            <span className="btn__content">join_</span>
+            <span className="btn__glitch"></span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
