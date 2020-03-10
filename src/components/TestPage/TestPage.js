40f18b3f479cd98e3731b8beedb02053719c3c03
Sophia Koeut <koeutsophia@gmail.com>
2020-03-10 15:41:19 -0700

    adjusted landing page header to fit all screens

--- src/components/TestPage/TestPage.js
+++ src/components/TestPage/TestPage.js
@@ -7,29 +7,29 @@ class TestPage extends Component {
     render() {
         return (
             <div>
-                <div class='container'>
-                    <div class="container_content">
-                        <div class="container_content_inner">
-                            <div class="title">
+                <div className='container'>
+                    <div className="container_content">
+                        <div className="container_content_inner">
+                            <div className="title">
                                 <h1>Hello</h1>
                             </div>
-                            <div class="par">
+                            <div className="par">
                                 <p>
                                     My name is Sophia Koeut and I love to convert tea into code to make the web beautiful!
                                 </p>
                             </div>
-                            <div class="btns">
-                                <button class='btns_more' augmented-ui="tl-clip br-clip exe">works</button>
+                            <div className="btns">
+                                <button className='btns_more' augmented-ui="tl-clip br-clip exe">works</button>
                             </div>
                         </div>
                     </div>
-                    <div class="container_outer_img" augmented-ui="tl-clip br-clip exe">
-                        <div class="img-inner">
-                            <img src={cyberpunk} alt="cyber punk girl and boy" class="container_img" />
+                    <div className="container_outer_img" augmented-ui="tl-clip br-clip exe">
+                        <div className="img-inner">
+                            <img src={cyberpunk} alt="cyber punk girl and boy" className="container_img" />
                         </div>
                     </div>
                 </div>
-                <div class="overlay"></div>
+                <div className="overlay"></div>
             </div>
         )
     }
